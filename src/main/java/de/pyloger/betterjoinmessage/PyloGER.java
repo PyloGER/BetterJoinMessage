8077cb26970ff5b29acce838bc218606e1f84d92
PyloGER <81117842+PyloGER@users.noreply.github.com>
2021-07-26 23:02:42 +0200

    Update

--- src/main/java/de/pyloger/betterjoinmessage/PyloGER.java
+++ src/main/java/de/pyloger/betterjoinmessage/PyloGER.java
@@ -11,9 +11,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PyloGER extends JavaPlugin implements Listener {
 
-    String prefix = getConfig().getString("config.prefix");
-    String suffix = getConfig().getString("config.suffix");
-
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -37,40 +34,101 @@ public final class PyloGER extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
 
         int online = Bukkit.getOnlinePlayers().size();
-        String prefix = getConfig().getString("config.prefix");
-        String suffix = getConfig().getString("config.suffix");
 
-        if(player.hasPlayedBefore()) {
-            if (getConfig().getBoolean("config.joinactivated", true)) {
+        boolean jm = getConfig().getBoolean("config.joinactivate");
+        boolean pm = getConfig().getBoolean("config.pmactivate");
+
+        String prefix = getConfig().getString("config.prefixjoin");
+        if (prefix != null) {
+            prefix = prefix.replace("%Player%", player.getName());
+            prefix = prefix.replace("%Online%", String.valueOf(online));
+        }
+        String suffix = getConfig().getString("config.suffixjoin");
+        if (suffix != null) {
+            suffix = suffix.replace("%Player%", player.getName());
+            suffix = suffix.replace("%Online%", String.valueOf(online));
+        }
+        String prefix2 = getConfig().getString("config.prefixpm");
+        if (prefix2 != null) {
+            prefix2 = prefix2.replace("%Player%", player.getName());
+            prefix2 = prefix2.replace("%Online%", String.valueOf(online));
+        }
+        String suffix2 = getConfig().getString("config.suffixpm");
+        if (suffix2 != null) {
+            suffix2 = suffix2.replace("%Player%", player.getName());
+            suffix2 = suffix2.replace("%Online%", String.valueOf(online));
+        }
+
+        if (jm) {
+            if (player.hasPlayedBefore()) {
                 String message1 = getConfig().getString("config.joinmessage");
-                message1 = message1.replace("%Player%", player.getName());
-                message1 = message1.replace("%Online%", String.valueOf(online));
-                event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', prefix + "§f " + message1 + "§f " + suffix));
-            }
-        } else if (getConfig().getBoolean("config.joinactivated", true)) {
-            String message = getConfig().getString("config.firstjoin");
-            message = message.replace("%Player%", player.getName());
-            message = message.replace("%Online%", String.valueOf(online));
-            event.setJoinMessage(ChatColor.translateAlternateColorCodes('&',prefix + "§f " + message + "§f " + suffix));
+                if (message1 != null) {
+                    message1 = message1.replace("%Player%", player.getName());
+                    message1 = message1.replace("%Online%", String.valueOf(online));
+                    event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', prefix + "§f " + message1 + "§f " + suffix));
+                }
+
             } else {
+                String message = getConfig().getString("config.firstjoin");
+                if (message != null) {
+                    message = message.replace("%Player%", player.getName());
+                    message = message.replace("%Online%", String.valueOf(online));
+                    event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', prefix + "§f " + message + "§f " + suffix));
+                }
+            }
+        }
+        if (!jm) {
             event.setJoinMessage(null);
         }
-        }
-    @EventHandler
-    public void onQuit(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-
-        int online = Bukkit.getOnlinePlayers().size()-1;
-        String prefix = getConfig().getString("config.prefix");
-        String suffix = getConfig().getString("config.suffix");
-
-        if (getConfig().getBoolean("config.quitactivated", true)) {
-        String message = getConfig().getString("config.quitmessage");
-        message = message.replace("%Player%", player.getName());
-        message = message.replace("%Online%", String.valueOf(online));
-        event.setQuitMessage(ChatColor.translateAlternateColorCodes('&',prefix + "§f " + message + "§f " + suffix));
-      } else {
-            event.setQuitMessage(null);
+        if (pm) {
+            if (player.hasPlayedBefore()) {
+                String message2 = getConfig().getString("config.privatemsg");
+                if (message2 != null) {
+                    message2 = message2.replace("%Player%", player.getName());
+                    message2 = message2.replace("%Online%", String.valueOf(online));
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix2 + message2 + suffix2));
+                } else {
+                    String message3 = getConfig().getString("config.firstpm");
+                    if (message3 != null) {
+                        message3 = message3.replace("%Player%", player.getName());
+                        message3 = message3.replace("%Online%", String.valueOf(online));
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix2 + message3 + suffix2));
+                    }
+                }
+            }
         }
     }
-}
+            @EventHandler
+            public void onQuit (PlayerQuitEvent event){
+                Player player = event.getPlayer();
+
+                int online = Bukkit.getOnlinePlayers().size() - 1;
+
+                boolean qm = getConfig().getBoolean("config.quitactivate");
+
+                String prefix = getConfig().getString("config.prefixquit");
+                if (prefix != null) {
+                    prefix = prefix.replace("%Player%", player.getName());
+                    prefix = prefix.replace("%Online%", String.valueOf(online));
+                }
+
+                String suffix = getConfig().getString("config.suffixquit");
+                if (suffix != null) {
+                    suffix = suffix.replace("%Player%", player.getName());
+                    suffix = suffix.replace("%Online%", String.valueOf(online));
+                }
+
+
+                if (qm) {
+                    String message = getConfig().getString("config.quitmessage");
+                    if (message != null) {
+                        message = message.replace("%Player%", player.getName());
+                        message = message.replace("%Online%", String.valueOf(online));
+                    }
+                    event.setQuitMessage(ChatColor.translateAlternateColorCodes('&', prefix + message + suffix));
+                }
+                if (!qm) {
+                    event.setQuitMessage(null);
+                }
+            }
+        }
