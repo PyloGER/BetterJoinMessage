1b983e20144c896b2006862f36fbbb52c28671f5
PyloGER <81117842+PyloGER@users.noreply.github.com>
2021-07-26 15:40:59 +0200

    Initial commit

---
+++ src/main/java/de/pyloger/betterjoinmessage/PyloGER.java
@@ -0,0 +1,35 @@
+package de.pyloger.betterjoinmessage;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PyloGER extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        loadConfig();
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    public void loadConfig() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String message = getConfig().getString("config.message");
+        message = message.replace("[Player]", player.getName());
+        event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+}
